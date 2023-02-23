3744b1ff8c122365a54b5d0bc103180336204709
Crxshxr <103609586+Crxshxr@users.noreply.github.com>
2023-02-23 16:53:15 -0500

    "go"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+import Calculation from './components/Calculation';
+import Result from './components/Result';
 
 function App() {
+  const [enteredNumbers, setEnteredNumbers] = useState({ first: 0, second: 0 });
+  const [chosenOperation, setChosenOperation] = useState('add'); // valid values: 'add', 'subtract', 'multiply', 'divide'
+
+  function changeFirstNumberHandler(event) {
+    setEnteredNumbers((prevNumbers) => ({
+      first: +event.target.value,
+      second: prevNumbers.second,
+    }));
+  }
+
+  function changeSecondNumberHandler(event) {
+    setEnteredNumbers((prevNumbers) => ({
+      first: prevNumbers.first,
+      second: +event.target.value,
+    }));
+  }
+
+  function updateOperationHandler(event) {
+    setChosenOperation(event.target.value);
+  }
+
+  let result;
+
+  if (chosenOperation === 'add') {
+    result = enteredNumbers.first + enteredNumbers.second;
+  } else if (chosenOperation === 'subtract') {
+    result = enteredNumbers.first - enteredNumbers.second;
+  } else if (chosenOperation === 'multiply') {
+    result = enteredNumbers.first * enteredNumbers.second;
+  } else {
+    result = enteredNumbers.first / enteredNumbers.second;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Calculation
+        onFirstNumberChanged={changeFirstNumberHandler}
+        onSecondNumberChanged={changeSecondNumberHandler}
+        onOperationChanged={updateOperationHandler}
+      />
+      <Result calculationResult={result} />
+    </>
   );
 }
 
